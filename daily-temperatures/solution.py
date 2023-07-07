8651f9ff377302a8a90e515585e3f27fdef9041a
Isak Larsson <isaklarsson98@hotmail.com>
2023-07-07 11:21:07 +0200

    was so close

--- daily-temperatures/solution.py
+++ daily-temperatures/solution.py
@@ -1,24 +1,15 @@
 class Solution:
     def dailyTemperatures( temperatures ): 
-        ans = []
-        checked_temps=[]
-        i = 0
-        while len(temperatures) != 0:
-            checked_temps.append(temperatures[i])
-            i += 1
-            if i == len(temperatures):
-                while i > 0:
-                    ans.append(0)
-                    i -= 1
-                return ans
-            if temperatures[i] > checked_temps[0]:
-                ans.append(len(checked_temps))
-                temperatures.pop(0)
-                checked_temps = []
-                i = 0
-                continue
+        res = [0] * len(temperatures)
+        stack = []
+        
+        for i,t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT,stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+            stack.append([t, i])
+        return res
             
-        return ans
 
 temperatures = [73,74,75,71,69,72,76,73]
 Solution.dailyTemperatures(temperatures=temperatures)
